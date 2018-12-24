658749ab8492ef696acb2e703962538146eac4fa
Michael Lin <linmichael02@gmail.com>
2018-12-24 17:26:22 -0500

    UNFINISHED: Adding BCH-ABC variant

---
+++ bitcoin-core/bitcoin_auto.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+# Preamble
+from nodetools import getIP, getAddr, conn
+# from os import system
+import random as rng
+from subprocess import check_output
+from time import time, sleep
+from math import floor
+from data_collection import minFee, medFee, memPool
+
+'''
+This script automates data collection and transaction gossip.
+Network must be bootstrapped first, see bootstrap.py.
+
+There are a total of ~156816000 transactions in this experiment of 5 days with 120 TPS periods.
+720 blocks will be mined on top of the 200000 initial reward generation, yielding 4720 blocks.
+
+Use jgarzik/python-bitcoinrpc for making JSON-RPC calls.
+
+1 Bitcoin (BTC) = 10^8 Satoshis
+3600 seconds = 1 hour (TPS interval)
+600 seconds = 10 minutes (Time between blocks)
+432000 seconds = 5 days (Experiment length)
+
+This version requires the command node to be large enough to handle
+120 RPC requests per second, plus dependent variable I/O.
+'''
+
+def main():
+    # Seed the RNG through GnuPG
+    rng.seed(check_output(['gpg', '-a', '--gen-random', '1', '32']))
+    # Set local experiment start time as Epoch
+    genesis = int(time())
+    # Set end time as Epoch
+    timeout = genesis+432000
+    while int(time()) <= timeout:
+        # The input of query() is equivalent to the current TPS.
+        nodeboth = query((floor((int(time())-genesis)/3600+1)))
+        print("From %s to %s" % nodeboth)
+        # 600 seconds is the block time, mine if 600 seconds have passed.
+        if (int(time())-genesis) % 600 == 0:
+            # Mine 1 block, store miner ID
+            nodemine = mine()
+            print("Mined by %s" % str(nodemine))
+            sleep(1)
+            # Query miner node what the block count is, check if a multiple of 6
+            if (conn(nodemine).getblockchaininfo()["blocks"]) - 200001 % 6 == 0:
+                # Get median fee of previous completed TPS interval
+                AtomMedFee = open("AtomMedFee.csv", "a")
+                AtomMinFee = open("AtomMinFee.csv", "a")
+                MemPool = open("MemPool.csv", "a")
+                AtomMedFee.write("%s,%s,bitcoin-mini\n" % (int(time()-genesis),
+                                                           medFee(floor((int(time()-genesis)/3600)-1))))
+                AtomMinFee.write("%s,%s,bitcoin-mini\n" %
+                                 (int(time()-genesis), minFee()))
+                MemPool.write("%s,%s,bitcoin-mini\n" %
+                              (int(time()-genesis), memPool()))
+                AtomMedFee.close()
+                AtomMinFee.close()
+                MemPool.close()
+            else:
+                # Skip median fee
+                AtomMinFee = open("AtomMinFee.csv", "a")
+                MemPool = open("MemPool.csv", "a")
+                AtomMinFee.write("%s,%s,bitcoin-mini\n" %
+                                 (int(time()-genesis), minFee()))
+                MemPool.write("%s,%s,bitcoin-mini\n" %
+                              (int(time()-genesis), memPool()))
+                AtomMinFee.close()
+                MemPool.close()
+        elif (int(time())-genesis) % 10 == 0:
+            # Skip median fee
+            AtomMinFee = open("AtomMinFee.csv", "a")
+            MemPool = open("MemPool.csv", "a")
+            AtomMinFee.write("%s,%s,bitcoin-mini\n" %
+                             (int(time()-genesis), minFee()))
+            MemPool.write("%s,%s,bitcoin-mini\n" %
+                          (int(time()-genesis), memPool()))
+            AtomMinFee.close()
+            MemPool.close()
+        #If not a multiple of 10 or 600 seconds, skip and query again.
+
+
+def query(tps):
+    match = True
+    while match:
+        # Generate node IDs
+        nodesend = rng.randint(1, 1000)
+        noderecv = rng.randint(1, 1000)
+        if nodesend != noderecv:
+            match = False
+            print(tps)
+            # Wait for the inverse of TPS period, or tps^-1
+            sleep(1/tps)
+            # Query nodesend to send 5460 satoshis (the dust limit) to noderecv
+            conn(nodesend).sendtoaddress(getAddr(noderecv), 0.00005460)
+            return (str(nodesend), str(noderecv))
+
+
+def mine():
+    nodemine = rng.randint(1, 1000)
+    conn(nodemine).generatetoaddress(1,getAddr(nodemine))
+    return nodemine
+
+
+if __name__ == "__main__":
+    main()
---
+++ bitcoin-core/bootstrap.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# Preamble
+import random as rng
+from nodetools import getIP, getAddr, getPriv, conn
+from os import system
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+'''
+This script bootstraps the network.
+1. Give each node their vanity private key.
+2. Have node 1 mine 200000 blocks.
+3. Send 9800 BTC to every node.
+'''
+
+
+def main():
+    for i in range(1, 1001):
+        # Import respective private keys to nodes
+        conn(i).importprivkey(getPriv(i))
+        # Set peer list to 0
+        # Connect node to 8 peers
+        peers = rng.sample(range(1,1001),8)
+        for peer in peers:
+            conn(i).addnode(getIP(peer)+':8333','add')
+            print("%s ==> %s" % (str(i), str(peer)))
+    # Mine 200000 blocks to node 1, 199900 blocks of immediately spendable rewards, 9995000 BTC.
+    # Uncomment next 4 lines if not on pregen image 
+    # for i in range(1, 2001):
+    #    conn(1).generatetoaddress(
+    #        100, 'mooo1TVU7edAhZNiwFAdjNarvgXQXsZYSh')
+    #    print("Block %s00" % i)
+    # Endow each address with 9800 BTC.
+    # Each transaction has 10 outputs, 100 transactions total.
+    for i in range(0, 1000, 10):
+        conn(1).sendmany("", {getAddr(i+1): 9800, getAddr(i+2): 9800, getAddr(i+3): 9800, getAddr(i+4): 9800, getAddr(
+            i+5): 9800, getAddr(i+6): 9800, getAddr(i+7): 9800, getAddr(i+8): 9800, getAddr(i+9): 9800, getAddr(i+10): 9800})
+    conn(1).generatetoaddress(
+            1, 'mooo1TVU7edAhZNiwFAdjNarvgXQXsZYSh')
+
+
+if __name__ == "__main__":
+    main()
